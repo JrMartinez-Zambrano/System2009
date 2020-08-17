cea2e5d021541db074e74923f99915a017fe46e7
JrMartinez-Zambrano <51100407+JrMartinez-Zambrano@users.noreply.github.com>
2020-08-16 18:44:30 -0600

    Primer commit

---
+++ Sistema.Datos/Conexion.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Data.SqlClient
+Public Class Conexion
+    Private _Base As String
+    Private _Servidor As String
+    Private _Usuario As String
+    Private _Clave As String
+    Private _Seguridad As Boolean = True
+    Public conn As SqlConnection
+
+    Public Property Base As String
+        Get
+            Return _Base
+        End Get
+        Set(value As String)
+            _Base = value
+        End Set
+    End Property
+
+    Public Property Servidor As String
+        Get
+            Return _Servidor
+        End Get
+        Set(value As String)
+            _Servidor = value
+        End Set
+    End Property
+
+    Public Property Usuario As String
+        Get
+            Return _Usuario
+        End Get
+        Set(value As String)
+            _Usuario = value
+        End Set
+    End Property
+
+    Public Property Clave As String
+        Get
+            Return _Clave
+        End Get
+        Set(value As String)
+            _Clave = value
+        End Set
+    End Property
+
+    Public Property Seguridad As Boolean
+        Get
+            Return _Seguridad
+        End Get
+        Set(value As Boolean)
+            _Seguridad = value
+        End Set
+    End Property
+
+    Public Sub New()
+        Me.Base = "dbsistema"
+        Me.Servidor = "DESKTOP-MJBMP9K\SQLEXPRESS"
+        Me.Usuario = ""
+        Me.Clave = ""
+        Me.conn = ""
+
+    End Sub
+End Class
