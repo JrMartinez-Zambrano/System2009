b5ae21a4b9b0979ca3a216a2ffa7f9e1092479f8
JrMartinez-Zambrano <51100407+JrMartinez-Zambrano@users.noreply.github.com>
2020-08-16 19:45:09 -0600

    Clase de Categoría en la capa de negocios

--- Sistema.Negocio/NCategoria.vb
+++ Sistema.Negocio/NCategoria.vb
@@ -1,5 +1,85 @@
 ﻿Imports Microsoft.VisualBasic
+Imports Sistema.Datos
+Imports Sistema.Entidades
 
-Public Class Class1
+Public Class NCategoria
+    Public Function Listar() As DataTable
+        Try
+            Dim Datos As New Dcategoria
+            Dim Tabla As New DataTable
+            Tabla = Datos.Listar()
+            Return Tabla
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return Nothing
+        End Try
+    End Function
+
+    Public Function Buscar(Valor As String) As DataTable
+        Try
+            Dim Datos As New Dcategoria
+            Dim Tabla As New DataTable
+            Tabla = Datos.Buscar(Valor)
+            Return Tabla
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return Nothing
+        End Try
+    End Function
+
+    Public Function Insertar(Obj As Categoria) As Boolean
+        Try
+            Dim Datos As New Dcategoria
+            Datos.Insertar(Obj)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Function Actualizar(Obj As Categoria) As Boolean
+        Try
+            Dim Datos As New Dcategoria
+            Datos.Actualizar(Obj)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Function Eliminar(Id As Integer) As Boolean
+        Try
+            Dim Datos As New Dcategoria
+            Datos.Eliminar(Id)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Function Desactivar(Id As Integer) As Boolean
+        Try
+            Dim Datos As New Dcategoria
+            Datos.Desactivar(Id)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Function Activar(Id As Integer) As Boolean
+        Try
+            Dim Datos As New Dcategoria
+            Datos.Activar(Id)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
 
 End Class
